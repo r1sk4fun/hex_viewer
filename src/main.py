c986c18f1200c793d8b99527bf04ed9658a7bf55
r1sk4fun <vl1.ermakovv@gmail.com>
2024-02-13 22:22:47 +0300

    Изменения Переписаны функции сборки строки; Теперь файл читается блоками; Добавлен скролл; Добавлена нумерация;

--- src/main.py
+++ src/main.py
@@ -9,35 +9,67 @@ class GUI(QMainWindow, main_window.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.browseFile.triggered.connect(self.browse_file)
+
+        self.BLOCK_WIDTH = 16
+        self.BLOCK_HEIGHT = 32
+        self.BLOCK_SIZE = self.BLOCK_WIDTH * self.BLOCK_HEIGHT
+        self.file_path: str | None = None
+
+        self.openFile.triggered.connect(self.open_file)
+        self.closeFile.triggered.connect(self.close_file)
         self.browseGitHub.triggered.connect(lambda: webbrowser.open('https://github.com/r1sk4fun/binary_viewer.git'))
+        self.blockScrollBar.valueChanged.connect(self.show_new_block)
 
 
-    def browse_file(self):
+    def show_new_block(self):
+        self.textView.clear()
+        self.read_by_block()
+
+
+    def setblockSpinMaximum(self):
+        self.blockScrollBar.setMaximum(os.path.getsize(self.file_path) // self.BLOCK_SIZE)
+
+
+    def open_file(self):
         file_path = QFileDialog.getOpenFileName(self, 'Open File', 'C:/')
         if file_path[0]:
-            self.textEdit.setText(F"Path to selected file: {file_path[0]}")
-            with open(file_path[0], 'rb') as file:
-                for step in range(0, os.path.getsize(file_path[0]), 16):
-                    bytes_data = file.read(16)
-                    output_line = ""
-                    for byte_counter, byte in enumerate(bytes_data):
-                        output_line += "{:02x}".format(byte, 2) + " "
-                        if (byte_counter + 1) % 16 == 0:
-                            output_line += "| " + self.readable_bytes(bytes_data)
-                            # real time update of GUI
-                            # drastically increases execution time
-                            QApplication.processEvents()
-                    # if EOF is reached then pad the output line
-                    if step + 16 >= os.path.getsize(file_path[0]):
-                        output_line += " " * ((3 * (16 - len(bytes_data)))) + "|" + " "
-                        output_line += self.readable_bytes(bytes_data)
-                    self.textEdit.append(output_line)
+            self.file_path = file_path[0]
+            self.setblockSpinMaximum()
+            self.textView.clear()
+            self.read_by_block()
 
 
-    def readable_bytes(self, bytes_data: bytes) -> str:
+    def close_file(self):
+        self.textView.clear()
+
+
+    def read_by_block(self):
+        if not self.file_path:
+            return None
+        with open(self.file_path, 'rb') as file:
+            file.seek(self.blockScrollBar.value() * self.BLOCK_SIZE)
+            block = file.read(self.BLOCK_SIZE)
+        rows = [block[i:i + self.BLOCK_WIDTH] for i in range(0, len(block), self.BLOCK_WIDTH)]
+        for num, row in enumerate(rows, start=1):
+            self.textView.append(self.show_number_of_bytes(num) + self.show_bytes(row) + self.show_line(row))
+
+
+    def show_number_of_bytes(self, num: int) -> str:
+        return "{:08x}".format((num * 16) + (self.blockScrollBar.value() * self.BLOCK_SIZE)) + ": "
+
+
+    def show_bytes(self, row: bytes) -> str:
         output_line = ""
-        for readable_byte in bytes_data:
+        for byte in row:
+            output_line += "{:02x}".format(byte, 2) + " "
+        if len(row) < self.BLOCK_WIDTH:
+            output_line += " " * (self.BLOCK_WIDTH - len(row)) * 3
+        return output_line + "| "
+
+
+    def show_line(self, row: bytes) -> str:
+        output_line = ""
+        for readable_byte in row:
             if readable_byte >= 32 and readable_byte <= 126:
                 output_line += chr(readable_byte)
             else:
