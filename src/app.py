a05fb4810d347dc3a92936fd739dd1e9238be086
r1sk4fun <vl1.ermakovv@gmail.com>
2024-02-14 23:39:10 +0300

    Добавлены поясняющие комментарии

--- src/app.py
+++ src/app.py
@@ -17,7 +17,8 @@ class GUI(QMainWindow, ui.Ui_MainWindow):
 
         self.openFile.triggered.connect(self.open_file)
         self.closeFile.triggered.connect(self.close_file)
-        self.browseGitHub.triggered.connect(lambda: webbrowser.open('https://github.com/r1sk4fun/binary_viewer.git'))
+        self.browseGitHub.triggered.connect(lambda: webbrowser
+                                            .open('https://github.com/r1sk4fun/binary_viewer.git'))
         self.scrollBar.valueChanged.connect(self.scroll_handler)
 
 
@@ -51,12 +52,17 @@ class GUI(QMainWindow, ui.Ui_MainWindow):
         with open(self.file_path, 'rb') as file:
             file.seek(self.scrollBar.value() * (2 * 16))
             block = file.read(self.BLOCK_SIZE)
+        # split block into rows
         rows = [block[i:i + self.BLOCK_WIDTH] for i in range(0, len(block), self.BLOCK_WIDTH)]
         for num, row in enumerate(rows):
-            self.textView.appendPlainText(self.show_bytes_quantity(num) + self.show_bytes(row) + self.show_printable_bytes(row))
+            self.textView.appendPlainText(self.show_bytes_quantity(num) + 
+                                          self.show_bytes(row) + 
+                                          self.show_printable_bytes(row))
 
 
     def show_bytes_quantity(self, num: int) -> str:
+        # (num * 16) for current block offset
+        # (self.scrollBar.value() * (2 * 16)) for current scrollbar offset
         return "{:08x}".format((num * 16) + (self.scrollBar.value() * (2 * 16))).upper() + ": "
 
 
@@ -65,6 +71,7 @@ class GUI(QMainWindow, ui.Ui_MainWindow):
         for byte in row:
             output_line += "{:02x}".format(byte) + " "
         if len(row) < self.BLOCK_WIDTH:
+            # each byte is 3 characters long (2 hex characters + 1 space)
             output_line += " " * (self.BLOCK_WIDTH - len(row)) * 3
         return output_line.upper() + "| "
 
@@ -72,6 +79,7 @@ class GUI(QMainWindow, ui.Ui_MainWindow):
     def show_printable_bytes(self, row: bytes) -> str:
         output_line = ""
         for byte in row:
+            # printable ASCII characters
             if byte >= 32 and byte <= 126:
                 output_line += chr(byte)
             else:
