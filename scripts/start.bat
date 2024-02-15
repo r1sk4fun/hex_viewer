16cc6add52e46b747d2548133651e9e6c70bc321
r1sk4fun <vl1.ermakovv@gmail.com>
2024-02-15 13:24:27 +0300

    Поправлен скрипт в соответствии с последними изменениями

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,12 +1,4 @@
 @echo off
 @cd /d "%~dp0\.."
 
-IF NOT EXIST .venv (
-    python -m venv .venv
-)
-
-call .\.venv\Scripts\activate.bat
-python.exe -m pip install --upgrade pip
-pip3 install -r requirements.txt
-cls
 python .\src\app.py
